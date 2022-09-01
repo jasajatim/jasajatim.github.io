723e56736383a4dfd2d756e9202905a06c603725
jasajatim <112391309+jasajatim@users.noreply.github.com>
2022-09-01 11:44:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".galeri-klien-slider", {
+  loop:true, 
+  grabCursor:true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
